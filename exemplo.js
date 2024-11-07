dc2da37ff32761014d8a46ac5af96e059447ad20
Dionis Moreira <59125498+dionismoreirapro@users.noreply.github.com>
2024-11-06 23:07:06 -0300

    Update exemplo.js

--- exemplo.js
+++ exemplo.js
@@ -7,7 +7,7 @@ export const options ={
     thresholds:{
         checks: ['rate > 0.99'], // range maior que 99%
         http_req_failed: ['rate < 0.01'],
-        http_req_duration: ['p(95) < 600']
+        http_req_duration: ['p(95) > 600']
 
     }
 }
